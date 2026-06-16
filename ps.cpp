9632057804649c5454f99beb315e42a21b208ac9
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2026-06-16 06:34:05 +0530

    Printing the process ids - Changes needed

---
+++ ps.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<filesystem>
+
+namespace fs = std::filesystem;
+
+using namespace std;
+
+int main(){
+    string path = "/proc";
+    for(const auto &entry : fs::directory_iterator(path)){
+        string entry_path = entry.path();
+        string temporary_path;
+        for(int i=6; i<entry_path.size(); i++){
+                int ascii_value_of_character = (int)entry_path[i];
+                if(ascii_value_of_character >= 49 and ascii_value_of_character <= 57){
+                        temporary_path += entry_path[i];
+                }
+                else
+                        break;
+        }
+        if(temporary_path.size() != 0){
+                cout<<temporary_path<<endl;
+        }
+    }
+}
