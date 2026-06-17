f2f11f9e5b6219ed0ed8a49910c4305b0ea4ab33
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2026-06-17 06:00:05 +0530

    Print PID alone with manual manipulation for getting process ids

--- ps.cpp
+++ ps.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main(){
     string path = "/proc";
+    cout<<"PID"<<endl<<endl;
     for(const auto &entry : fs::directory_iterator(path)){
         string entry_path = entry.path();
         string temporary_path;
         for(int i=6; i<entry_path.size(); i++){
                 int ascii_value_of_character = (int)entry_path[i];
-                if(ascii_value_of_character >= 49 and ascii_value_of_character <= 57){
+                if(ascii_value_of_character >= 48 and ascii_value_of_character <= 57){
                         temporary_path += entry_path[i];
                 }
                 else
